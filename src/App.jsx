deff656956885a9ed826edbd184f2fb2fd1ceef6
Jerick Sebree <jcsebree.com>
2025-10-20 17:10:08 -0400

    fix: adding logo and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Routes, Route, Link, useLocation, useNavigate } from "react-router-dom";
 import InterviewPage from "./interview/InterviewPage.jsx";
 import LandingPage from "./landing/LandingPage.jsx";
-import LogoLight from "./assets/SceneMe_black_icon_transparent.png"; // black logo for light mode
-import LogoDark from "./assets/SceneMe_white_icon_transparent.png"; // white logo for dark mode
+import LogoLight from "./assets/SceneMe_black_text_transparent.png"; // black logo with text for light mode
+import LogoDark from "./assets/SceneMe_white_text_transparent.png"; // white logo with text for dark mode
 
 function AppHeader() {
   const location = useLocation();
